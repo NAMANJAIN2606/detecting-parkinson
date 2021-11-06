bdf66627c7257a743e91a7fc592ea909a870ea65
NAMAN JAIN <namanjainmail@gmail.com>
2021-11-07 00:40:15 +0530

    Add files via upload

---
+++ detectingParkinson/detectingParkinson.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import os, sys
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+#DataFlair - Read the data
+df=pd.read_csv('parkinsons.data')
+features=df.loc[:,df.columns!='status'].values[:,1:]
+labels=df.loc[:,'status'].values
+
+scaler=MinMaxScaler((-1,1))
+x=scaler.fit_transform(features)
+y=labels
+
+x_train,x_test,y_train,y_test = train_test_split(x, y, test_size=0.2, random_state=7)
+
+
+model = XGBClassifier()
+model.fit(x_train, y_train)
+
+
+# DataFlair - Calculate the accuracy
+y_pred=model.predict(x_test)
+print(accuracy_score(y_test, y_pred)*100)
